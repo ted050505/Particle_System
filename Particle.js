c08da192f38b754d1ca5258081aeedbf537f03c9
Jung Jun Hyeok <ted0505@naver.com>
2021-11-05 18:06:12 +0900

    fix class Particle

--- Particle.js
+++ Particle.js
@@ -1,5 +1,5 @@
 class Particle {
-  constructor() {
+  constructor(x, y) {
     this.pos = createVector(x, y);
     this.vel = createVector(0, 0);
     this.acc = createVector(0, 0);
--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
+let particle;
+
 function setup() {
   createCanvas(400, 400);
+  particle = new Particle(width/2, height/2);
 }
 
 function draw() {
-  background(220);
+  background(0);
+
+  let gravity = createVector(0, 0.2);
+  particle.applyForce(gravity);
+  particle.edges();
+  particle.update();
+  particle.display();
 }
