0e9f276f458beaaca3efb4b3c443b5d81b2e2036
Jung Jun Hyeok <ted0505@naver.com>
2021-11-05 18:35:04 +0900

    add loop Particle

--- sketch.js
+++ sketch.js
@@ -2,13 +2,14 @@ let particles = [];
 
 function setup() {
   createCanvas(400, 400);
-  for(let i=0;i<100;i++) {
-    particles.push(new Particle(width/2, height/2));
-  }
 }
 
 function draw() {
   background(0);
+
+  for(let i=0;i<1;i++) {
+    particles.push(new Particle(width/2, height/2));
+  }
 
   for(let particle of particles) {
     let gravity = createVector(0, 0.2);
