daf6a23a8ce9ef3f5b4c26ef306d229e2315cae1
Jung Jun Hyeok <ted0505@naver.com>
2021-11-05 17:30:32 +0900

    add Particle.js

---
+++ Particle.js
@@ -0,0 +1,41 @@
+class Particle {
+  constructor() {
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.r = 15;
+  }
+
+  applyForce(force) {
+    this.acc.add(force);
+  }
+
+  edges() {
+    if (this.pos.y >= height - this.r) {
+      this.pos.y = height - this.r;
+      this.vel.y *= -1;
+    }
+
+    if (this.pos.x >= width - this.r) {
+      this.pos.x = width - this.r;
+      this.vel.x *= -1;
+    } else if (this.pos.x <= this.r) {
+      this.pos.x = this.r;
+      this.vel.x *= -1;
+    }
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+  }
+
+  display() {
+    stroke(255);
+    strokeWeight(2);
+    fill(255);
+
+    ellipse(this.pos.x, this.pos.y, this.r * 2);
+  }
+}
