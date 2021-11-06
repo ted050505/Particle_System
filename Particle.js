5d6e45b87c33ed0bbecb95826866ee3aa19d48b2
Jung Jun Hyeok <ted0505@naver.com>
2021-11-06 11:26:52 +0900

    emitter array

--- Particle.js
+++ Particle.js
@@ -1,11 +1,10 @@
-class Particle {
+class Particle extends p5.Vector {
   constructor(x, y) {
-    this.pos = createVector(x, y);
+    super(x, y);
     this.vel = p5.Vector.random2D();
     this.vel.mult(random(0.5, 2));
     this.acc = createVector(0, 0);
     this.r = 5;
-
     this.lifetime = 255;
   }
 
@@ -17,35 +16,19 @@ class Particle {
     this.acc.add(force);
   }
 
-  edges() {
-    if (this.pos.y >= height - this.r) {
-      this.pos.y = height - this.r;
-      this.vel.y *= -1;
-    }
-
-    if (this.pos.x >= width - this.r) {
-      this.pos.x = width - this.r;
-      this.vel.x *= -1;
-    } else if (this.pos.x <= this.r) {
-      this.pos.x = this.r;
-      this.vel.x *= -1;
-    }
-  }
-
   update() {
     this.vel.add(this.acc);
-    this.pos.add(this.vel);
+    this.add(this.vel);
     this.acc.set(0, 0);
 
     this.lifetime -= 5;
   }
 
   display() {
-    // stroke(255, this.lifetime);
     noStroke();
     strokeWeight(2);
     fill(255, this.lifetime);
 
-    ellipse(this.pos.x, this.pos.y, this.r * 2);
+    ellipse(this.x, this.y, this.r * 2);
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,16 +1,22 @@
-let emitter;
+let emitters = [];
 // let particles = [];
+
+function mousePressed() {
+  emitters.push(new Emitter(mouseX, mouseY));
+  emitters.update_color();
+}
 
 function setup() {
   createCanvas(400, 400);
-  emitter = new Emitter(200, 20);
+  emitters[0] = new Emitter(200, 20);
 }
 
 function draw() {
   background(0);
 
-  emitter.emit(5);
-
-  emitter.update();
-  emitter.display();
+  for(let emitter of emitters) {
+    emitter.emit(5);
+    emitter.update();
+    emitter.display();
+  }
 }
